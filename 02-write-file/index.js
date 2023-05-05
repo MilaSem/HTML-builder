b53035b34ade9fb1b84219c87b36e32a12d3088e
MilaSem <milafoxsem@gmail.com>
2023-05-05 19:38:28 +0300

    feat: solve '02-write-file' problem

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileLocation = path.join(__dirname, 'text.txt');
+
+const stream = fs.createWriteStream(fileLocation);
+
+process.stdout.write('Весь текст из командной строки будет сохранён в text.txt\nДля выхода введи exit\nНапиши свой текст:\n');
+
+process.stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') {
+    toCloseApp();
+  }
+  stream.write(data);
+});
+
+const toCloseApp = function() {
+  process.stdout.write('Текст записан. Пока!');
+  process.exit();
+}
