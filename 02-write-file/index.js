c96a73f9b10c8bd26326b382d98c0e659a1f7fe7
MilaSem <milafoxsem@gmail.com>
2025-01-20 02:36:47 +0300

    refactor: change 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,24 +1,26 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const fileLocation = path.join(__dirname, 'text.txt');
+const { stdin, stdout } = process;
+
+const fileLocation = path.join(__dirname, "text.txt");
 
 const stream = fs.createWriteStream(fileLocation);
 
-process.stdout.write('Весь текст из командной строки будет сохранён в text.txt\nДля выхода введи exit\nНапиши свой текст:\n');
+stdout.write(
+  "All text from the command line will be saved in text.txt\nTo terminate the app type 'exit'\nEnter your text:\n"
+);
 
-process.stdin.on('data', data => {
-  if (data.toString().trim() === 'exit') {
+stdin.on("data", (data) => {
+  if (data.toString().trim() === "exit") {
     toCloseApp();
   }
   stream.write(data);
 });
 
-process.on('SIGINT', () => {
-  toCloseApp();
-});
-
-const toCloseApp = function() {
-  process.stdout.write('Текст записан. Пока!');
+const toCloseApp = () => {
+  stdout.write("\nThe text is recorded. Bye!");
   process.exit();
-}
+};
+
+process.on("SIGINT", toCloseApp);
