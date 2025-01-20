c56a765850ced4267f36cf7ef0cc6a195fa2f36b
MilaSem <milafoxsem@gmail.com>
2025-01-20 18:16:19 +0300

    refactor: change 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,26 +1,29 @@
-const { access } = require('fs');
-const fsPromises = require('fs/promises');
-const path = require('path');
+const fs = require("node:fs/promises");
+const path = require("node:path");
 
-const dirStyleLocation = path.join(__dirname, 'styles');
-const fileStyleLocation = path.join(__dirname, 'project-dist', 'bundle.css');
+const dirStyleLocation = path.join(__dirname, "styles");
+const fileStyleLocation = path.join(__dirname, "project-dist", "bundle.css");
 
-async function mergeStyles() {
+const mergeStyles = async () => {
   try {
-    await fsPromises.writeFile(fileStyleLocation, '');
-    const styles = await fsPromises.readdir(dirStyleLocation, {withFileTypes: true});
-    for (const style of styles) {
-      if (style.isFile()) {
-        const styleFileName = style.name.split('.');
-        if (styleFileName[1] === 'css') {
-          const styleComponent = await fsPromises.readFile(path.join(dirStyleLocation, style.name), {encoding: 'utf8'});
-          await fsPromises.appendFile(fileStyleLocation, styleComponent);
-        }
-      }
-    }
-  } catch(err) {
-    console.error(err.message);
+    await fs.writeFile(fileStyleLocation, "");
+
+    const styles = await fs.readdir(dirStyleLocation, { withFileTypes: true });
+
+    const stylePromises = styles
+      .filter((style) => style.isFile() && path.extname(style.name) === ".css")
+      .map((style) =>
+        fs.readFile(path.join(dirStyleLocation, style.name), {
+          encoding: "utf8",
+        })
+      );
+
+    const styleComponents = await Promise.all(stylePromises);
+
+    await fs.appendFile(fileStyleLocation, styleComponents.join("\n"));
+  } catch (err) {
+    process.stderr.write(`Error ${err.message}\n`);
   }
-}
+};
 
 mergeStyles();
